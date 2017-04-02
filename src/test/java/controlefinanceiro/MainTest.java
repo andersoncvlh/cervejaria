a1669620282c1083406329daf31bd6eb4edecc37
anderson carvalho martins <anderson.cvlh@gmail.com>
2017-04-02 10:29:04 -0300

    Testes com a nova classe Optional

---
+++ src/test/java/controlefinanceiro/MainTest.java
@@ -0,0 +1,17 @@
+package controlefinanceiro;
+
+import java.util.Optional;
+
+/**
+ * aprendendo nova classe java8: Optional
+ * 
+ * @author anderson
+ *
+ */
+public class MainTest {
+
+	public static void main(String[] args) {
+		Optional<String> textos = Optional.ofNullable("anderson");
+	}
+	
+}
